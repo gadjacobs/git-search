6324462557114c448670c5824379870e0d6f1436
gadjacobs <gadexofficial@gmail.com>
2018-10-20 19:38:27 +0100

    Added state and modified UI

--- src/Container/App.js
+++ src/Container/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import User from '../Components/User';
+import RepoList from '../Components/RepoList';
 import Search from '../Components/Search';
 import 'tachyons';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      // Initial state for the list of repositoies and the search box.
+      repos: [],
+      searchBox: ''
+    };
   }
 
   render() {
-    return (
+    const { repos, searchBox } = this.state;
+
+    return repos.length === 0 ? (
+      <article className="vh-100 dt w-100 bg-dark-pink">
+        <div className="dtc v-mid tc white ph3 ph4-l">
+          <h2 className="f6 f2-m f-subheadline-l fw6 tc">LOADING!!!</h2>
+        </div>
+      </article>
+    ) : (
       <div className="tc">
         <h1>Github Repo Finder</h1>
         <h4 className="white-90">
@@ -22,9 +34,7 @@ class App extends Component {
 
         <h1>Repos:</h1>
 
-        <User />
-        <User />
-        <User />
+        <RepoList />
       </div>
     );
   }
